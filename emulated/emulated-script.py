71b5c00277451d5cb0d5e51357efd1dfc720f7f5
Cswl C <12976647+cswl@users.noreply.github.com>
2020-05-15 06:34:51 +0545

    CI: Use Pre installed software

--- emulated/emulated-script.py
+++ emulated/emulated-script.py
@@ -6,63 +6,72 @@ import requests
 from pathlib import Path
 from shell import shell
 
+from tempfile import gettempdir
 
-ci_run_dir = Path.home() / "ci_run"
+
+# Check if we are running locally or in Github CI
+
+temp = Path.home()  if  'GITHUB_ACTIONS' in os.environ else Path(gettempdir())  
+ci_run_dir =   temp / "ci_run" 
+ 
 os.makedirs(ci_run_dir, exist_ok=True)
 
 ci_steps_dir = ci_run_dir / "steps"
 os.makedirs(ci_steps_dir, exist_ok=True)
 
 TERMUX_FDROID_URL = "https://f-droid.org/repo/com.termux_92.apk"
-
-
 TERMUX_APK = ci_run_dir / "termux_92.apk"
 
 # Currently we sleep for an arbitary amount of time
 # Until we find an exact intent? to listen to
 
-app_sleep_steps = [15, 10]
 
-
-def main():
+def step_install_termux(adb, steps_sleep ):
     print(f" Downloading.....  {TERMUX_FDROID_URL}")
-
     r = requests.get(TERMUX_FDROID_URL)
     with open(TERMUX_APK, "wb") as f:
         f.write(r.content)
-    adb = AdbRunner(app_sleep_steps)
-    adb.get_emulator_info()
-
+ 
+    adb.screencap_step(steps_sleep['before_termux_install']  )
     print(" Installing....")
-    adb.install(TERMUX_APK)
-    adb.start_app("com.termux")
 
-    adb.screencap_step()
-    adb.sleep()
+    adb.install(TERMUX_APK)
+    adb.screencap_step(steps_sleep['after_termux_install'] )
+
+def step_install_tsu(adb, step_sleep):
+ 
+    adb.start_app("com.termux")
+    adb.screencap_step(5)
 
     adb.input_text("pkg install tsu")
     adb.input_key(23)
-    adb.screencap_step()
-    adb.sleep()
+    adb.screencap_step(2)
+ 
     adb.input_text("tsu --dbg")
     adb.input_key(23)
+    adb.screencap_step(3)
+
+def step_emulator_info(adb):
+    adb.get_emulator_info()
+    adb.screencap_step()
+
+def main():
+    adb = AdbRunner()
+    step_install_termux(adb, {
+     'before_termux_install' :  10,
+     'after_termux_install' : 5
+    })
+    step_install_tsu(adb, {} )
+
 
 
 """
 We already have an adb connected device from the runner action
 """
-
-
 class AdbRunner:
-    def __init__(self, wait):
+    def __init__(self):
         self.scount = 1
-        self.step_wait = wait
-
-    def sleep(self):
-        print(self.step_wait)
-        s = self.step_wait[self.scount - 2]
-        time.sleep(s)
-
+ 
     def run_cmd(self, cmd):
         print(">>>", cmd)
         c = shell(cmd)
@@ -72,9 +81,11 @@ class AdbRunner:
     def shell(self, args):
         shell(f"adb shell {args}")
 
-    def screencap_step(self):
+    def screencap_step(self, steps_sleep):
+        time.sleep(steps_sleep)
         self.run_cmd(f"adbe screenshot  {ci_steps_dir}/step_{self.scount}.png")
         self.scount += 1
+
 
     def get_emulator_info(self):
         self.run_cmd("adbe devices")
